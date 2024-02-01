509d19f0a882b68777357612eb3cd6754b4b5083
Sample Projects <99012157+mzunohkaru@users.noreply.github.com>
2024-02-01 19:09:32 +0900

    second commit

--- threads_clone/threads_clone/Core/Components/Threads/View/ContentActionButtonsView.swift
+++ threads_clone/threads_clone/Core/Components/Threads/View/ContentActionButtonsView.swift
@@ -91,3 +91,34 @@ struct ContentActionButtonsView_Previews: PreviewProvider {
         ContentActionButtonsView(thread: dev.thread)
     }
 }
+
+/*
+@StateObject var viewModel: ThreadDetailsViewModel
+
+init(thread: Thread) {
+    self.thread = thread
+    self._viewModel = StateObject(wrappedValue: ThreadDetailsViewModel(thread: thread))
+}
+
+・ @StateObjectは、プロパティが初期化されるときに一度だけオブジェクトを作成します
+・ このプロパティラッパーは、そのそのビューが所有する状態を管理するために使用されます
+・ ビューがその状態を所有している場合、またはそのビューがそのオブジェクトの唯一の所有者である場合に使用します
+・ ビューが再描画されても、@StateObjectによって保持されるオブジェクトは破棄されず、同じインスタンスが保持され続けます
+
+ユースケース : ビューが表示されるときにビューモデルを初期化し、ビューが破棄されるまでその状態を保持したい場合
+*/
+
+/*
+@ObservedObject var viewModel: ContentActionButtonsViewModel
+
+init(thread: Thread) {
+    self.viewModel = ContentActionButtonsViewModel(thread: thread)
+}
+
+・ @ObservedObjectは、外部から渡されるオブジェクトを参照するために使用されます
+・ このプロパティラッパーは、ビューが所有していないが、変更を監視する必要があるオブジェクトに使用されます
+・ ビューの外部で作成され、ビューに渡されるオブジェクトを参照する場合に使用します
+・ ビューが再描画されると、@ObservedObjectが参照するオブジェクトは変わらない可能性がありますが、オブジェクトの所有権はビューにはありません
+
+ユースケース : 親ビューから子ビューにビューモデルを渡す場合や、複数のビュー間で共有されるビューモデルを参照する場合に使用します
+*/
